327bdc1f87a2a6f786ba5674c709732804792f25
Al <alboudreau1994@gmail.com>
2021-12-13 09:31:13 -0500

    commit

---
+++ Helpers/Extensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helpers
+{
+    public static class Extensions
+    {
+        public static int[,] ConvertTo2dIntArray(this List<string> data)
+        {
+            var grid = new int[data.Count, data[0].Length];
+            // row
+            for (int i = 0; i < data.Count; i++)
+            {
+                // column
+                for (int j = 0; j < data[i].Length; j++)
+                {
+                    grid[i, j] = int.Parse(data[i][j].ToString());
+                }
+            }
+
+            return grid;
+        }
+    }
+}
